98c38fa0f610bdea4b05da6efad97facfc4c330a
marcloh108 <marc.loh@molisys.com>
2019-11-15 15:40:12 +0700

    Write a program in C that checks whether the elements in an unsorted array appears
    Create functions to define and print
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question4/main.c
+++ question4/main.c
@@ -1,28 +1,67 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <stdbool.h>
 
-int findMaximum(int arr[], int n)
+int FindMin(int arr1[], int n);
+int FindMax(int arr1[], int n);
+
+bool areConsecutive(int arr1[], int n)
 {
-    int max_ele = arr[0];
-    for(int i = 1; i < n; i++)
-    {
-        if(arr[i] > max_ele)
-            max_ele = arr[i];
-    }
-    return max_ele;
+  if ( n <  1 )
+    return false;
+  int min_no = FindMin(arr1, n);
+  int max_no = FindMax(arr1, n);
+  if (max_no - min_no  + 1 == n)
+  {
+      bool *checked = (bool *) calloc (n, sizeof(bool));
+      int i;
+      for (i = 0; i < n; i++)
+      {
+         if ( checked[arr1[i] - min_no] != false )
+           return false;
+         checked[arr1[i] - min_no] = true;
+      }
+      return true;
+  }
+  return false;
+}
+
+int FindMin(int arr1[], int n)
+{
+  int min_no = arr1[0];
+  for (int i = 1; i < n; i++)
+   if (arr1[i] < min_no)
+     min_no = arr1[i];
+  return min_no;
+}
+
+int FindMax(int arr1[], int n)
+{
+  int max_no = arr1[0];
+  for (int i = 1; i < n; i++)
+   if (arr1[i] > max_no)
+     max_no = arr1[i];
+  return max_no;
 }
 
 int main()
 {
-    int arr[] = {2, 7, 12, 25, 4, 57, 27, 44};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int i;
-    printf("The given array is: \n");
-    for(i=0;i<n;i++)
-    {
-        printf("%d ", arr[i]);
-    }
+    int arr1[]= {7, 4, 3, 5, 6, 2};
+      int i;
+      int arr_size = sizeof(arr1)/sizeof(arr1[0]);
+   //------------- print original array ------------------
+    printf("The given array is:  \n");
+    for(i = 0; i < arr_size; i++)
+        {
+            printf("%d  ", arr1[i]);
+        }
     printf("\n");
-    printf("The maximum element which is increasing then decreasing is %d", findMaximum(arr, n));
-    getchar();
+//-----------------------------------------------------------
+    int n = sizeof(arr1)/sizeof(arr1[0]);
+    if(areConsecutive(arr1, n) == true)
+        printf("The appearence of elements in the array are consecutive.");
+    else
+        printf("The appearence of elements in the array are not consecutive.");
     return 0;
 }
+
