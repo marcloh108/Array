47505f2e4745234fea0814002f3ad56e2ea10952
marcloh108 <marc.loh@molisys.com>
2019-11-15 11:34:00 +0700

    Write a program in C for a 2D array of size 3x3 and print the matrix
    Create functions for condition and printing
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question4/main.c
+++ question4/main.c
@@ -2,46 +2,27 @@
 
 void main()
 {
-    int arr1[100], fr1[100];
-    int i,j, n, ctr;
+    int arr1[3][3],i,j;
 
+    printf("\n\nRead a 2D array of size 3x3 and print the matrix :\n");
+    printf("------------------------------------------------------\n");
 
-       printf("\n\nCount frequency of each element of an array :\n");
-       printf("-------------------------------------------------\n");
-
-       printf("Input the number of elements to be stored in the array :");
-       scanf("%d",&n);
-
-       printf("Input %d elements in the array :\n",n);
-       for(i=0;i<n;i++)
-        {
-          printf("element - %d : ",i);
-          scanf("%d",&arr1[i]);
-          fr1[i] = -1;
-        }
-
-    for(i=0; i<n; i++)
+    printf("Input elements in the matrix :\n");
+    for(i=0;i<3;i++)
     {
-        ctr = 1;
-        for(j=i+1; j<n; j++)
+        for(j=0;j<3;j++)
         {
-            if(arr1[i]==arr1[j])
-            {
-                ctr++;
-                fr1[j]=0;
-            }
-        }
-        if(fr1[i]!=0)
-        {
-            fr1[i] = ctr;
+            printf("element - [%d],[%d] : ", i, j);
+            scanf("%d", &arr1[i][j]);
         }
     }
-    printf("\nThe frequency of all elements of array : \n");
-    for(i=0; i<n; i++)
+
+    printf("\nThe matrix is : \n");
+    for(i=0;i<3;i++)
     {
-        if(fr1[i]!=0)
-        {
-            printf("%d occurs %d times\n", arr1[i], fr1[i]);
-        }
+        printf("\n");
+        for(j=0;j<3;j++)
+            printf("%d\t",arr1[i][j]);
     }
+    printf("\n\n");
 }
