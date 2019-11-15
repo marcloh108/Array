af99351129ee1d6ef6bb148dfeb946a6e3b36d6e
marcloh108 <marc.loh@molisys.com>
2019-11-15 15:08:41 +0700

    Write a program in C to find the maximum element in an array which is first increasing and then decreasing
    Create function to define and print
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question4/main.c
+++ question4/main.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
 
+int findMaximum(int arr[], int n)
+{
+    int max_ele = arr[0];
+    for(int i = 1; i < n; i++)
+    {
+        if(arr[i] > max_ele)
+            max_ele = arr[i];
+    }
+    return max_ele;
+}
+
 int main()
 {
-    int i, j, array[10][10], sum, rows, columns;
-
-    printf("\nEnter the number of Rows : ");
-    scanf("%d", &rows);
-
-    printf("\nEnter the number of Columns : ");
-    scanf("%d", &columns);
-
-    for(i=0;i<rows;i++)
-        for(j=0; j<columns; j++)
-        {
-            printf("\nEnter the Element array[%d][%d] : ", i, j);
-            scanf("%d", &array[i][j]);
-        }
-
-    sum = 0;
-    for (i = 0; i < rows; i++)
-        for( j = 0; j < columns; j++)
-        {
-            if (i < j)
-            {
-                sum = sum + array[i][j];
-            }
-        }
-    printf("\nSum of Upper Triangle Elements : %d", sum);
-    return (0);
+    int arr[] = {2, 7, 12, 25, 4, 57, 27, 44};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i;
+    printf("The given array is: \n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    printf("The maximum element which is increasing then decreasing is %d", findMaximum(arr, n));
+    getchar();
+    return 0;
 }
